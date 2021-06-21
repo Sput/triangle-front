3397dcd28591a33126531b580d6224601f06b90b
Sput <paulknick@gmail.com>
2021-06-21 08:51:56 -0400

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer bg-dark">
+            <div className="container text-center">
+                <span className="text-muted">@2021 Written by us, built by SEI 412 (during COVID)</span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
